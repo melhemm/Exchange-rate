80d7342309bead255685bb21fcc8c63601f1f87f
melhemm <jamalmulhemm@gmail.com>
2022-05-18 21:18:54 +0300

    add redux and create req

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
-import { Homepage } from './components/Homepage';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { Home } from './components/Home/Home';
+import { MovieDeatils } from './components/MovieDeatils/MovieDeatils'
+import { PageNotFound } from './components/PageNotFound/PageNotFound'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div className='app'>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDeatils} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+     <Footer/>
     </div>
   );
 }

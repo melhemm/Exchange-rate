4594d47011a1227b2b195218ab60f99041f8c79b
melhemm <jamalmulhemm@gmail.com>
2022-05-18 21:52:37 +0300

    add link to movies deatils

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const MovieCard = (props) => {
   const { data } = props
   return (
-    <div>
-      <h4>{data.Title} Year {data.Year}</h4>
-      <img src={data.Poster} alt={data.title} />
-      
-    </div>
+    <Link to={`/movie/${data.imdbID}`}>
+      <div>
+        <h4>{data.Title} Year {data.Year}</h4>
+        <img src={data.Poster} alt={data.title} />
+      </div>
+    </Link>
+   
   )
 }
